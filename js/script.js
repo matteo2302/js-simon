866cda9e98e959fbfb0608fbb744e48185c63e66
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-09 16:15:13 +0200

    NaN problems

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 console.log("jsok");
 //fase 0
-let countDown = document.querySelector("h2");
-let number = document.querySelector("p");
+let countDown = document.querySelector("h1");
+let number = document.querySelector("li");
+let listNumber = document.querySelector("ul");
+let answers = [];
 let numbers = [];
 while (numbers.length < 5) {
   const randoNumber = Math.floor(Math.random() * 100) + 1;
+  listNumber.innerHTML += `<li> ${randoNumber} </li>`;
   numbers.push(randoNumber);
 }
 console.table(numbers);
+countDown.innerText = 30;
+setInterval(function () {
+  countDown.innerText = countDown--;
+  console.log(countDown);
+}, 1000);
--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 
 **fase 0**
 
-- creo la stuttura enl dom che avrà un h2 e 5 p
-- collego l'h2 e le p in js
-- creare un array di numeri e un ciclo while che pusha 5 numeri dentro casuali
+- creo la stuttura enl dom che avrà un h2 e un ul
+- collego l'h2 e l'ul in js
+- creare un array di numeri e un ciclo while che pusha 5 numeri dentro casuali e crea 5 li con quei numeri
 - creo un array di numeri giusti
   **fase 1**
 - collego a un evento temporale h2 che partira da un numero pari a 30 fino a 0 e dopodiche inserisco un if che ferma l'evento nel caso nell'h2 ci sia scritto 0
